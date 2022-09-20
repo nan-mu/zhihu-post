fb1297157c5586831e1b114c4c64656bc99277cb
nan-mu <mu.nan@88.com>
2022-09-20 19:31:04 +0800

    完成文章预览页面中文章总数的获取

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ import { deepClone } from "./utils/index.js"
 import Service from "./utils/service.js"
 
 const __dirname = resolve();
-const { outputFile, readFile } = fs;
+const { outputFile, readFile, writeJSON } = fs;
 
 const root_url = "https://www.zhihu.com/people/maluyelang666/posts";
 
 const once_req = async () => {
     let data = deepClone(await Service({ url: root_url }));
     to_file("temp/once_file.html", data, "finish once req and save in temp/once_file.html");
-    //await fs.writeJson("temp/once_file.json", JSON.stringify(data));
-    //console.log(Object.keys(data));
 }
 
 const to_file = async (_path, data, mes = "save a file") => {
@@ -27,9 +25,19 @@ const to_file = async (_path, data, mes = "save a file") => {
 }
 
 const load_art = async () => {
-    //await once_req();
-    let $ = load(await readFile(join(__dirname, "temp/once_file.html"), "utf8"));
-    console.log(typeof $(".Pagination button").text());
+    try {
+        await once_req();
+        let $ = load(await readFile(join(__dirname, "temp/once_file.html"), "utf8"));
+        let art_len = -1, temp = -1, reg = /\d+/;
+        $(".Pagination button").each((i, elem) => {
+            if (reg.test($(elem).text())) {
+                temp = Number($(elem).text());
+                art_len = art_len < temp ? temp : art_len;
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 load_art();
